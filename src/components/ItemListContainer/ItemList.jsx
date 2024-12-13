7533e2a8f11a9e6a458a64aee7cede655eb64b9a
Leonel Fernandez <lfernandez@ddm.com.ar>
2024-12-13 12:35:32 -0300

    Actualización automática

--- src/components/ItemListContainer/ItemList.jsx
+++ src/components/ItemListContainer/ItemList.jsx
@@ -4,7 +4,7 @@ const ItemList = ({products}) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
     {products.map((product) => {
-        <Item product={product}/>
+        <Item product={product} key={product.id} />
     })}
     </div>
   )
